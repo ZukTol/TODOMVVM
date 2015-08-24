2c42d2d3fc6d252994949f3ee6ab8c187dacf9f3
ZukTol <sldancer@yandex.ru>
2015-08-24 10:31:26 +0300

    1

--- TODOMVVM/ViewModels/MainWindowViewModel.cs
+++ TODOMVVM/ViewModels/MainWindowViewModel.cs
@@ -8,5 +8,16 @@ using Caliburn.Micro;
 namespace TODOMVVM.ViewModels {
     public class MainWindowViewModel : Screen {
         public override string DisplayName { get { return "TODOMVVM"; } set { } }
+
+        #region Public properties
+
+        public string NewToDoText {
+            get { return _newToDoText; }
+            set {
+                _newToDoText = value;
+                NotifyOfPropertyChange("NewToDoText");
+            }
+        } private string _newToDoText;
+        #endregion Public properties
     }
 }

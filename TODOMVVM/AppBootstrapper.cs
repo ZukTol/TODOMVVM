eb345339611aaf690392aa0ba85884807ef630df
ZukTol <sldancer@yandex.ru>
2015-08-24 08:37:09 +0300

    Внедрил калибурн, сделал хэдер

---
+++ TODOMVVM/AppBootstrapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using TODOMVVM.ViewModels;
+
+namespace TODOMVVM {
+    public class AppBootstrapper : BootstrapperBase {
+        SimpleContainer _container;
+
+        public AppBootstrapper() {
+            Initialize();
+        }
+
+        protected override void Configure() {
+            _container = new SimpleContainer();
+
+            _container.Singleton<IWindowManager, WindowManager>();
+            _container.Singleton<IEventAggregator, EventAggregator>();
+
+            _container.Singleton<MainWindowViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key) {
+            var instance = _container.GetInstance(service, key);
+            if (instance != null)
+                return instance;
+
+            throw new InvalidOperationException("Could not locate any instances.");
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service) {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance) {
+            _container.BuildUp(instance);
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e) {
+            DisplayRootViewFor<MainWindowViewModel>();
+        }
+    }
+}
---
+++ TODOMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace TODOMVVM.ViewModels {
+    public class MainWindowViewModel : Screen {
+        public override string DisplayName { get { return "TODOMVVM"; } set { } }
+    }
+}
